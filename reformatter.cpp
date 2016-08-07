a8a01be71851c7b2a9e4cf7415618d323a76e32f
junaidiiith <junaid695683@gmail.com>
2016-08-07 23:52:37 +0530

    Removed sqlite dependency

--- reformatter.cpp
+++ reformatter.cpp
@@ -8,52 +8,64 @@
 #include <sqlite3.h>
 #include <stdlib.h>
 #include <sstream>
+
+
+#include <cstdio>
+#include <cstdlib>
+#include <libgen.h>
+#include <string>
+#include <getopt.h>
 using namespace std;
 
 map<int,string> opening_tags;
 map<int, string> closing_tags;
 
 void make_maps(string filename)
-{	
-	sqlite3 *db;
-	sqlite3_open(filename.c_str(), & db);
-	string selectQuery = "SELECT * FROM TAGS_DATA;";
-    sqlite3_stmt *selectStmt;
-  
-  	if(sqlite3_prepare_v2(db, "select * from TAGS_DATA;", -1, &selectStmt, 0) != SQLITE_OK) cout << "Didn't Select Item!" << endl;
-  	else
-  	{
-    	int columns = sqlite3_column_count(selectStmt);
-     	int result = 0;
-     	while(true)
-     	{
-        	result = sqlite3_step(selectStmt);
-        	if(result == SQLITE_ROW)
-        	{
-           		
-          		stringstream s;
-          		s << sqlite3_column_text(selectStmt, 0);
-          		int id = stoi(s.str());
-
-          		stringstream o_tag;
-          		string opening_tag;
-          		o_tag << sqlite3_column_text(selectStmt, 1);
-          		opening_tag = o_tag.str();
-					
-				stringstream c_tag;
-          		string closing_tag;
-          		c_tag << sqlite3_column_text(selectStmt, 2);
-          		closing_tag = c_tag.str();
-
-          		opening_tags[id] = opening_tag;
-          		closing_tags[id] = closing_tag;
-
-        	}
-        	else
-           		break;
-     	}
-    	sqlite3_finalize(selectStmt);
-    }	
+{		
+	ifstream tags(filename.c_str());
+	string str((std::istreambuf_iterator<char>(tags)), std::istreambuf_iterator<char>());
+	int l = str.length();
+	string opening_tag = "";
+	string closing_tag = "";
+	string s_index;
+	bool left = true;
+	int i = 0;
+	int index;
+	cout << str << endl;
+	while(i < l)
+	{	
+		if(left)
+		{	
+			s_index = "";
+			index = -1;
+			while(str[i]!='=')
+				s_index += str[i++];
+			left = false;
+			i++;
+			index = atoi(s_index.c_str());
+			cout << "index is:" << index << endl;
+		}
+		else
+		{	
+			opening_tag = "";
+			closing_tag = "";
+			while(str[i]!='\0')
+			{	
+				if(str[i]==',')
+				{
+					i++;
+					while(str[i]!='\0')
+						closing_tag += str[i++];
+				}
+				else
+					opening_tag += str[i++];
+			}
+			i++;
+			opening_tags[index] = opening_tag;
+			closing_tags[index] = closing_tag;
+			left = true;
+		}
+	}
 }
 
 bool is_blank(char ch)
@@ -75,50 +87,112 @@ void empty_stack(stack<int> &mystack)
 	}
 	while(!temp.empty())
 	{	
-		//cout << "Closing tag with id ="<< temp.top() << endl;
 		cout << closing_tags[temp.top()];
 		temp.pop();
 	}
 }
 
+void usage(char *progname)
+{
+  wcerr << L"USAGE: " << basename(progname) << L" [input_file [output_file]]" << endl;
+  exit(EXIT_FAILURE);
+}
 
 int main(int argc, char **argv)
 {	
+	string str;
+	make_maps("tags_data.txt");
 
-	if(argc!=3)
-	{	
-		cout << "Insufficient arguments\n";
-		return 1;
+	if((argc-optind+1) > 3)
+	{
+	  usage(argv[0]);
 	}
 
-	make_maps(argv[2]);
-
-	ifstream in(argv[1]);
+  FILE *input, *output;
+  
+  if((argc-optind+1) == 1)
+  {
+    input = stdin;
+    output = stdout;
+    char mystring [100];
+    FILE *fp;
+    fp = fopen("input.xml","w+b");
+    while(fgets(mystring,100,stdin) != NULL)
+    {
+    	fputs(mystring,fp);
+    }
+    fclose(fp);	
+	ifstream in("input.xml");
 	std::string s((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+	str = s;
+
+  }
+  else if ((argc-optind+1) == 2)
+  {
+    input = fopen(argv[argc-1], "r");
+    if(!input)
+    {
+      usage(argv[0]);
+    }
+    output = stdout;
+  
+	  if(feof(input))
+	  {
+	    wcerr << L"ERROR: Can't read file '" << argv[1] << L"'" << endl;
+	    exit(EXIT_FAILURE);
+	  }
+	  ifstream in(argv[argc-1]);
+	  std::string s((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+	  str = s;
+  }
+  else
+  {
+    input = fopen(argv[argc-2], "r");
+    output = fopen(argv[argc-1], "w+b");
+
+    if(!input || !output)
+    {
+      usage(argv[0]);
+    }
+    if(feof(input))
+    {
+      wcerr << L"ERROR: Can't read file '" << argv[1] << L"'" << endl;
+      exit(EXIT_FAILURE);
+    }
+    ifstream in(argv[argc-2]);
+    std::string s((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+    str = s;
+  }
+
+
+#ifdef _MSC_VER
+    _setmode(_fileno(input), _O_U8TEXT);
+    _setmode(_fileno(output), _O_U8TEXT);
+#endif
+
 	
-	int l = s.length();
+	int l = str.length();
 	stack<int> mystack;
 	int i = 0;
 	while(i<l)
 	{	
-		if(((s[i] == '[' && i == 0) || (s[i] == '[' && s[i-1] != '\\')) && i+1 < l)
+		if(((str[i] == '[' && i == 0) || (str[i] == '[' && str[i-1] != '\\')) && i+1 < l)
 		{	
 			empty_stack(mystack);
-			if(s[i+1]=='{' && i+1 < l)
+			if(str[i+1]=='{' && i+1 < l)
 			{	
 				int j = i+2;
-				while(s[j] != '}')
+				while(str[j] != '}')
 				{	
 					string tag;
-					while(s[j]!=',' && s[j]!='}')
-						tag += s[j++];
-					//cout << "Tag is" << tag << "this\n";
+					while(str[j]!=',' && str[j]!='}')
+						tag += str[j++];
 					
-					int ind = stoi(tag);
+					int ind = atoi(tag.c_str());
 
 					mystack.push(ind);
 					
-					if(s[j] == '}')
+					if(str[j] == '}')
 						break;
 					else
 						j++;
@@ -137,11 +211,11 @@ int main(int argc, char **argv)
 				int j = i+1;
 				string tag;
 				bool blank_super = true;
-				while(s[j] != ']' && j < l)
+				while(str[j] != ']' && j < l)
 				{	
-					if(!is_blank(s[j]))
+					if(!is_blank(str[j]))
 						blank_super = false;
-					tag += s[j++];
+					tag += str[j++];
 				}
 				if(blank_super)
 				{	
@@ -149,7 +223,7 @@ int main(int argc, char **argv)
 				}
 				else
 				{	
-					int ind = stoi(tag);
+					int ind = atoi(tag.c_str());
 					cout << opening_tags[ind];
 				}
 				i = j+1;
@@ -157,10 +231,10 @@ int main(int argc, char **argv)
 		}
 		else
 		{
-			if(s[i] == '\\')
+			if(str[i] == '\\')
 				i++;
 			else
-				cout << s[i++];
+				cout << str[i++];
 		}
 	}
 
